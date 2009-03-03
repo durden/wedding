1626cc05678da5e16c5b0bc9e8d3f9cc5c9fbfaa
Luke Lee <durdenmisc@gmail.com>
2009-03-02 22:26:43 -0600

    Added unicode methods for printing to model classes

--- wedding_app/models.py
+++ wedding_app/models.py
@@ -5,18 +5,30 @@ class Page(models.Model):
 	text = models.TextField()
 	updated = models.DateField(auto_now=True)
 
+	def __unicode__(self):
+		return self.name
+
 class Rsvp(models.Model):
 	name = models.CharField(max_length=30)
 	guests = models.PositiveSmallIntegerField()
 	email = models.EmailField()
+
+	def __unicode__(self):
+		return self.name
 
 class Blog(models.Model):
 	updated = models.DateField(auto_now=True)
 	updated_by = models.CharField(max_length=20)
 	text = models.TextField()
 
+	def __unicode__(self):
+		return self.text
+
 class Comment(models.Model):
 	updated = models.DateField(auto_now=True)
 	updated_by = models.CharField(max_length=20)
 	text = models.TextField()
 	blog = models.ForeignKey(Blog)	
+
+	def __unicode__(self):
+		return self.text
