e6337cf5e432d146ece5211f216b0565eb7c6031
Luke Lee <durdenmisc@gmail.com>
2009-03-07 17:50:42 -0600

    Removed global urls array. The lnlinks are going to be fixed anyway.

--- views.py
+++ views.py
@@ -1,39 +1,28 @@
 from django.shortcuts import render_to_response
 from wedding.wedding_app.models import Blog, Page, Rsvp
 
-urls = {
-	'home' 			: '/',
-	'blog' 			: '/blog',
-	'about' 		: '/about',
-	'rsvp' 			: '/rsvp',
-	'contact' 		: '/contact',
-	'maps' 			: '/maps',
-	'registrations' : '/registrations',
-	'pictures' 		: '/pictures',
-}
-
 def home(request):
 	page = Page.objects.get(name="Home")
-	return render_to_response('page.html', {'urls' : urls, 'page' : page})
+	return render_to_response('page.html', {'page' : page})
 
 def blog(request):
 	blogs = Blog.objects.all().order_by("-updated")
-	return render_to_response('blog.html', {'urls' : urls, 'blogs' : blogs})
+	return render_to_response('blog.html', {'blogs' : blogs})
 
 def about(request):
 	page = Page.objects.get(name="About Us")
-	return render_to_response('page.html', {'urls' : urls, 'page' : page})
+	return render_to_response('page.html', {'page' : page})
 
 def rsvp(request):
 	if request.method != 'POST':
-		return render_to_response('rsvp.html', {'urls' : urls, 'status' : 0})
+		return render_to_response('rsvp.html', {'status' : 0})
 
 	first = request.POST.get('first', '')
 	last = request.POST.get('last', '')
 	num = request.POST.get('guests', '0')
 
 	if first == '' or last == '' or num == 0:
-		return render_to_response('rsvp.html', {'urls' : urls, 'status' : -1})
+		return render_to_response('rsvp.html', {'status' : -1})
 	else:
 		try:
 			rsvp = Rsvp.objects.get(first_name=first, last_name=last)
@@ -46,18 +35,18 @@ def rsvp(request):
 
 		rsvp.save()
 
-		return render_to_response('rsvp.html', {'urls' : urls, 'first' : first,
-						  'last' : last, 'guests' : num, 'status' : 1})
+		return render_to_response('rsvp.html', {'first' : first, 'last' : last,
+								  'guests' : num, 'status' : 1})
 
 def contact(request):
-	return render_to_response('contact.html', {'urls' : urls})
+	return render_to_response('contact.html')
 
 def maps(request):
-	return render_to_response('maps.html', {'urls' : urls})
+	return render_to_response('maps.html')
 
 def registrations(request):
 	page = Page.objects.get(name="Registration Places")
-	return render_to_response('page.html', {'urls' : urls, 'page' : page})
+	return render_to_response('page.html', {'page' : page})
 
 def pictures(request):
-	return render_to_response('pictures.html', {'urls' : urls})
+	return render_to_response('pictures.html')
