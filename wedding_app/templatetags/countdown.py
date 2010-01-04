fac7cf8e532f9cf61efa4c95129c48690d38af09
Luke Lee <durdenmisc@gmail.com>
2010-01-03 18:24:19 -0600

    Adding docstrings

--- wedding_app/templatetags/countdown.py
+++ wedding_app/templatetags/countdown.py
@@ -1,16 +1,24 @@
+"""Wedding date countdown template tag"""
+
 from django import template
 
 from datetime import date
 
 register = template.Library()
 
+
 class CountdownNode(template.Node):
+    """Node to handle countdown tag"""
+
     def render(self, context):
+        """Compute # days until wedding date and return as phrase"""
+
         wedding_date = date(2009, 10, 17)
         diff = wedding_date - date.today()
         return "%s days until wedding" % (diff.days)
 
+
 @register.tag(name="countdown")
 def countdown(parser, token):
+    """Countdown tag"""
     return CountdownNode()
-
