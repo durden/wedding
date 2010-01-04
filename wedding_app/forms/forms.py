a9f81ff5f46bcb38f2638305bf7d030316d9a90b
Luke Lee <durdenmisc@gmail.com>
2010-01-03 18:23:55 -0600

    Adding docstrings and other misc. pep8/pylint cleanups

--- wedding_app/forms/forms.py
+++ wedding_app/forms/forms.py
@@ -1,14 +1,24 @@
+"""Forms for wedding app"""
+
 from wedding.wedding_app.models import Rsvp
 from django import forms
 
+
 class RsvpForm(forms.ModelForm):
+    """RSVP form"""
+
     choices = (('Yes', 'Yes'), ('No', 'No'))
     attending = forms.BooleanField(widget=forms.RadioSelect(choices=choices))
     class Meta:
+        """Tie RSVP model to this form"""
         model = Rsvp
 
+
 class ContactForm(forms.Form):
+    """Contact form"""
+
     name = forms.CharField(max_length=100)
     subject = forms.CharField(max_length=100)
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows' : 10, 'cols': 60}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 10,
+                                                           'cols': 60}))
     email = forms.EmailField()
