db63211682699d6870837400217ca9e2757ff82e
Luke Lee <durdenmisc@gmail.com>
2010-01-03 18:35:30 -0600

    Cleaning up image template tags for pep8/pylint findings

--- wedding_app/templatetags/images.py
+++ wedding_app/templatetags/images.py
@@ -1,16 +1,21 @@
+"""Image template tags"""
+
 from django import template
 
 import random
 
 register = template.Library()
 
+
 class Image:
+    """Container class for image"""
+
     def __init__(self, name, width, height):
         self.name = name
         self.width = width
         self.height = height
 
-images = [
+IMAGES = [
     # Trying to order by size, not number
     Image('lukenatalie1.jpg', 208, 275),
     Image('lukenatalie9.jpg', 208, 275),
@@ -26,21 +31,33 @@ images = [
     Image('lukenatalie12.jpg', 308, 232),
 ]
 
+
 class RandomImageNode(template.Node):
+    """Node to handle random_image tag"""
+
     def render(self, context):
-        img = random.choice(images)
+        """Find random image and return html to display it"""
+
+        img = random.choice(IMAGES)
         html = '<img id="page_img" src="/wmedia/img/%s" width="%d" height="%d" alt="Luke and Natalie">' % (img.name, img.width, img.height)
         return html
 
+
 @register.tag(name="random_image")
-def get_image(parser, token):
+def get_random_image(parser, token):
+    """random_image template tag"""
     return RandomImageNode()
 
+
 class AllImagesNode(template.Node):
+    """Node to handle all_images template tag"""
+
     def render(self, context):
+        """Find all images and return html to display them as a list"""
+
         cnt = 0
         html = '<ul id="images">'
-        for img in images:
+        for img in IMAGES:
             img_tag = '<img src="/wmedia/img/%s" width="%d" height="%d" alt="Luke and Natalie">' % (img.name, img.width, img.height)
 
             if not cnt:
@@ -52,7 +69,8 @@ class AllImagesNode(template.Node):
         html = html + "</ul>"
         return html
 
-@register.tag(name="all_images")
-def get_image(parser, token):
-    return AllImagesNode()
 
+@register.tag(name="all_images")
+def get_all_images(parser, token):
+    """all_images template tag"""
+    return AllImagesNode()
